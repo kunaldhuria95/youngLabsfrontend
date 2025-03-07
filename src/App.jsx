28d9472dccabb925c641cd83db3175707199a536
kunal dhuria <kunaldhuria95@gmail.com>
2025-03-07 15:17:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+   
+  const handleForm = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.get(`http://localhost:3000/api/greet?name=${name}`);
+      setMessage(data.message);
+    } catch (error) {
+      setMessage(error.response.data.error)
+      
+    } 
+  }
+
+  return (
+    <section className='home'>
+      <h1>YoungLabs</h1>
+      <div className='mainContainer'>
+        <form onSubmit={handleForm} className='formContainer'>
+          <label htmlFor="myName">Your Name</label>
+          <input type='text' onChange={(e) => setName(e.target.value)} value={name} id='myName' />
+          <button>Get Greeting.</button>
+        </form>
+      </div>
+      <p>{message}</p>
+    </section>
+  )
+}
+
+export default App
